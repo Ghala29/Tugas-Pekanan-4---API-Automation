369ca6851618fc99f6b213587c8d01469eb7015a
Ghala29 <ghalasudana24@gmail.com>
2023-02-14 17:17:51 +0700

    modified:   Units.test.js

--- Units.test.js
+++ Units.test.js
@@ -1,168 +1,163 @@
+//Untuk Manggil config.js
 const { request, expect } = require("./config");
-
-describe("POST /authentications", function () {
-  it("Success login ", async function () {
+//Login Positive
+describe("POST /authentications", function (){
+  it("Success login ", async function (){
     const response = await request.post("/authentications")
-  
-      .send({ 
-          email: "ghalatest@gmail.com",
-          password: "Ghala2022"
-       
-           
+     .send({ 
+       email: "ghalatest@gmail.com",
+       password: "Ghala2022"
   })
+  //Expected Status Login
     expect(response.status).to.eql(201);
-    var data = response.body.data
+      var data = response.body.data
     expect(data.accessToken).to.be.a('string')
-   token = response.body.data.accessToken;
-   refreshToken = response.body.data.refreshToken;
-    });
+  //Untuk Manggil semua token pada transaksi      
+        token = response.body.data.accessToken;
+        refreshToken = response.body.data.refreshToken;
   });
-  describe("POST /authentications", function () {
+  });
+  //Login Negative Mandatory Dikosongkan
+  describe("POST /authentications", function (){
     it("Fail login ", async function () {
       const response = await request.post("/authentications")
-    
         .send({ 
-            email: "",
-            password: ""
-         
-             
-    })
+          email: "",
+          password: ""
+  })
+  //Expected Status Pada Login Negative
       expect(response.status).to.eql(400);
-      const data = response.body
+        const data = response.body
       expect(data.status).to.eql('fail')
       expect(data.message).to.eql('\"email\" is not allowed to be empty')
-
-      });
-    });
-
-describe("POST /units", function () {
-    it("Success Units - Add Unit ", async function () {
+  });
+  });
+  //Add Unit Positive
+  describe("POST /units", function (){
+    it("Success Units - Add Unit ", async function (){
       const response = await request.post("/units")
         .set("Authorization", `Bearer ${token}`)
         .send({ 
-        name: "Gula", 
-        description: "Manis"
-    });
-  
+          name: "Gula", 
+          description: "Manis"
+  });
+  //Expected Status Pada Add Unit Positive
       expect(response.status).to.eql(201);
-  
-      const body = response.body;
+        const body = response.body;
       expect(body.status).to.eql('success');
       expect(body.message).to.eql("Unit berhasil ditambahkan");
-      
-    
-      const dataUnit = response.body.data;
-      const isValid = isUUID(dataUnit.unitId);
+        const dataUnit = response.body.data;
+        const isValid = isUUID(dataUnit.unitId);
       expect(isValid).to.be.true;
       expect(dataUnit.name).to.eql("Gula");
-      uuid = response.body.data.unitId
-    });
-});
-function isUUID(str) {
-  const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-  return uuidPattern.test(str);
-}
-describe("POST /units", function () {
-  it("400 - Bad Request ", async function () {
+          uuid = response.body.data.unitId
+  });
+  });
+  //Function Untuk return uuidPattern
+    function isUUID(str) {
+    const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+    return uuidPattern.test(str);
+  }
+  //Negative Case Untuk Add Units Test
+describe("POST /units", function (){
+  it("400 - Bad Request ", async function (){
     const response = await request.post("/units")
       .set("Authorization", `Bearer ${token}`)
       .send({ 
-      name: "", 
-      description: ""
+        name: "", 
+        description: ""
   });
-
-    expect(response.status).to.eql(400);
-
-    const body = response.body;
+  //Expected Status Untuk Negative Case Add Units Test
+    expect(response.status).to.eql(400)
+      const body = response.body;
     expect(body.status).to.eql('fail');
     expect(body.message).to.eql("name is required, description is optional");
   });
-});
-describe("GET /units/${uuid}", function () {
-  it("Success Units - Get Unit Detail ", async function () {
+  });
+  //Positive case untuk get units detail test 
+describe("GET /units/${uuid}", function (){
+  it("Success Units - Get Unit Detail ", async function (){
     const response = await request.get(`/units/${uuid}`)
       .set("Authorization", `Bearer ${token}`)
       .send({ 
-       
   });
-
+  //Expected Status Untuk get unit detail test
     expect(response.status).to.eql(200);
-    const body = response.body;
+      const body = response.body;
     expect(body.status).to.eql('success');
-    const data = response.body.data.unit;
+      const data = response.body.data.unit;
     expect(data.name).to.eql("Gula");
     expect(data.description).to.eql("Manis");
   });
-});
-describe("GET /units?q=Gula&page=1", function () {
-    it("Success Units - Get Unit List ", async function () {
+  });
+//Positive Case untuk Get Unit List Test
+describe("GET /units?q=Gula&page=1", function (){
+  it("Success Units - Get Unit List ", async function (){
       const response = await request.get("/units?q=Gula&page=1")
         .set("Authorization", `Bearer ${token}`)
         .send({ 
-         
-    });
-  
-      expect(response.status).to.eql(200);
+  });
+//Expected Status untuk Get Unit List Test
+    expect(response.status).to.eql(200);
       const body = response.body;
-      expect(body.status).to.eql('success');
+    expect(body.status).to.eql('success');
       const unit = response.body.data.units[0];
       const isValid = isUUID(unit.id);
-      expect(isValid).to.be.true;
-      expect(unit.name).to.eql('Gula');
-      expect(unit.description).to.eql('Manis');
+    expect(isValid).to.be.true;
+    expect(unit.name).to.eql('Gula');
+    expect(unit.description).to.eql('Manis');
       const meta = response.body.data.meta;
-      expect(meta.totalPages).to.be.at.least(1);
-      expect(meta.total).to.be.at.least(1);
-      expect(meta.page).to.eql("1");
-    });
+    expect(meta.totalPages).to.be.at.least(1);
+    expect(meta.total).to.be.at.least(1);
+    expect(meta.page).to.eql("1");
   });
+  });
+//Function Untuk return uuidPattern
   function isUUID(str) {
     const uuidPattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
     return uuidPattern.test(str);
   }
-  describe("PUT /units/${uuid}", function () {
-    it("Success Units - Update Unit ", async function () {
+//Positive Case Untuk Update Units
+  describe("PUT /units/${uuid}", function (){
+    it("Success Units - Update Unit ", async function (){
       const response = await request.put(`/units/${uuid}`)
         .set("Authorization", `Bearer ${token}`)
         .send({ 
-          
-            name: "GulaUpdated",
-            description: "Ga Manis"
-        
-    });
-  
-      expect(response.status).to.eql(200);
-      const body = response.body;
-      expect(body.status).to.eql('success');
-      const data = response.body.data;
-      expect(data.name).to.eql("GulaUpdated");
-    });
+          name: "GulaUpdated",
+          description: "Ga Manis"
   });
-  describe("DELETE /units/${uuid}", function () {
-    it("Success Units - Delete Unit ", async function () {
+//Expected Status Untuk Update Unit  
+      expect(response.status).to.eql(200);
+        const body = response.body;
+      expect(body.status).to.eql('success');
+        const data = response.body.data;
+      expect(data.name).to.eql("GulaUpdated");
+  });
+  });
+//Positive Case Untuk Delete Units
+  describe("DELETE /units/${uuid}", function (){
+    it("Success Units - Delete Unit ", async function (){
       const response = await request.delete(`/units/${uuid}`)
         .set("Authorization", `Bearer ${token}`)
         .send({ 
-    });
-  
+  });
+//Expected Status untuk update status
       expect(response.status).to.eql(200);
       const body = response.body;
       expect(body.status).to.eql('success');
-    });
   });
-  describe("Delete /authentications", function () {
+  });
+//Positive Case Logout
+  describe("Delete /authentications", function (){
     it("Success Delete ", async function () {
       const response = await request.delete("/authentications")
       .send({ 
-        
         refreshToken:refreshToken
-
-             
-    });
-  
+  });
+//Expected Status Untuk Logout 
       expect(response.status).to.eql(200);
       const data = response.body
       expect(data.status).to.eql('success');
       expect(data.message).to.eql('Refresh token berhasil dihapus');
-      });
-    });
+  });
+  });
